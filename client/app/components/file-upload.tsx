b468baa1a10740435804d22f36705299d42dfcc0
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-04-21 16:47:23 +0530

    fix(client): correct component name and enhance file upload functionality
    - Renamed FileUplaoadComponent to FileUploadComponent for consistency.
    - Updated the file upload component to allow users to select and log PDF files.
    - Adjusted layout styles for better alignment and user experience.

--- client/app/components/file-upload.tsx
+++ client/app/components/file-upload.tsx
@@ -1,14 +1,35 @@
-'use-client'
-import * as React from 'react';
-import {Upload} from 'lucide-react'
-const FileUplaoadComponent: React.FC = () =>{
-    return (<div className='bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-lg '>
-        <div className='flex justify-center items-center flex-col'>
-            <h3>Upload a PDF file.</h3>
-            <Upload/>
-        </div>
-        
-    </div>);
+"use client";
+
+import * as React from "react";
+import { Upload } from "lucide-react";
+
+const FileUploadComponent: React.FC = () => {
+  const handleFileUploadBtnClick = () => {
+    const el = document.createElement("input");
+    el.setAttribute("type", "file");
+    el.setAttribute("accept", "application/pdf");
+
+    el.addEventListener("change", (event: Event) => {
+      const input = event.target as HTMLInputElement;
+      if (input.files && input.files.length > 0) {
+        console.log("Selected file:", input.files[0]);
+      }
+    });
+
+    el.click();
+  };
+
+  return (
+    <div className="bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-lg">
+      <div
+        onClick={handleFileUploadBtnClick}
+        className="flex justify-center items-center flex-col cursor-pointer"
+      >
+        <h3>Upload a PDF file.</h3>
+        <Upload />
+      </div>
+    </div>
+  );
 };
 
-export default FileUplaoadComponent;
+export default FileUploadComponent;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import FileUplaoadComponent from "./components/file-upload";
-
+import FileUploadComponent from "./components/file-upload";
 
 export default function Home() {
   return (
     <div>
       <div className="min-h-screen w-screen flex">
-        <div className="w-[30vw] min-h-screen">
-          <FileUplaoadComponent/>
+        <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center ">
+          <FileUploadComponent/>
         </div>
         <div className="w-[70vw] min-h-screen border-l-2">2</div>
       </div>
